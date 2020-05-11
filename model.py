ec06ebd0d4a3128272f0e0b002bd528f1a8f638c
Matej Bolta <matej.bolta@gmail.com>
2020-05-11 19:19:27 +0200

    updated model with all methods

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 os.chdir('UvP/Vislice')
 
 STEVILO_DOVOLJENIH_NAPAK = 10
@@ -13,48 +13,70 @@ ZMAGA = 'W'
 PORAZ = 'X'
 
 bazen_besed = []
-with open('besede.txt', encoding='utf-8') as datoteka_besed:
+with open('besede.txt', 'r', encoding='utf-8') as datoteka_besed:
     for beseda in datoteka_besed:
         bazen_besed.append(beseda.strip().lower())
 
-# class Igra:
-#     def __init__(self, geslo, crke=None):
-#         self.geslo = geslo
-#         if crke is None:
-#             self.crke = []
-#         else:
-#             self.crke = crke
-    
-#     def napacne_crke():
-#         pass
+ class Igra:
+    def __init__(self, geslo, crke=None):
+        self.geslo = geslo.lower()
+        if crke is None:
+            self.crke = []
+        else:
+            self.crke = [c.lower() for c in crke]
 
-#     def pravilne_crke():
-#         pass
+    def napacne_crke(self):
+        '''vrnet seznam napačnih ugibanj igralca'''
+        return [c for c in self.crke if c not in self.geslo]
+
+    def pravilne_crke(self):
+        '''vrnet seznam pravilnih ugibanj igralca'''
+        return [c for c in self.crke if c in self.geslo]
+
+    def stevilo_napak(self):
+        '''izračuna koliko napačnih ugibov je igralec že naredil'''
+        return len(self.napacne_crke)
+
+    def je_zmaga(self):
+        return all(c in self.crke for c in self.geslo)
+
+    def je_poraz(self):
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
+
+    def pravilni_del_gesla(self):
+        '''vrne niz z že uganjenim delom gesla, tako da neznane črke zamenja s podčrtajem'''
+        rtr = ''
+        for crka in self.geslo:
+            if crka in self.crke:
+                rtr += crka
+            else:
+                trenutno += '_'
+
+        return rtr
+
+    def nepravilni_ugibi(self):
+        '''vrne niz, ki vsebuje s presledkom ločene nepravilne ugibe igralca'''
+        return ' '.join(self.napacne_crke())
+
+    def ugibaj(self, ugibana_crka):
+        '''vrne primernega od PRAVILNA_CRKA, PONOVLJENA_CRKA, NAPACNA_CRKA, ZMAGA, PORAZ'''
+        ugibana_crka = ugibana_crka.lower()
+        if ugibana_crka in self.crke:
+            return PONOVLJENA_CRKA
+        
+        self.crke.append(ugibana_crka)
+
+        if ugibana_crka in self.geslo:
+            if self.je_zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else:
+            if self.je_poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
 
 
-
-
-# Metodi napacne_crke in pravilne_crke, ki vrneta seznam pravilnih
-#  oz. napačnih ugibanj igralca.
-
-# Metodo stevilo_napak, ki izračuna koliko napačnih ugibov 
-# je igralec že naredil.
-
-# Metodi zmaga in poraz, ki preverita ali trenutno stanje
-#  določa zmago oz. poraz.
-
-# Metodo pravilni_del_gesla, ki vrne niz z že uganjenim 
-# delom gesla, tako da neznane črke zamenja s podčrtajem.
-
-# Metodo nepravilni_ugibi, ki vrne niz, ki vsebuje s presledkom
-#  ločene nepravilne ugibe igralca.
-
-# Metodo ugibaj, ki sprejme črko, jo pretvori v veliko črko,
-#  in vrne primernega od PRAVILNA_CRKA, PONOVLJENA_CRKA, 
-#  NAPACNA_CRKA, ZMAGA, PORAZ.
-
-# V datoteki napišite kodo, ki iz datoteke besede.txt izlušči 
-# nabor besed in ga shrani v seznam bazen_besed.
-
-# Napišite funkcijo nova_igra, ki zgradi in vrne novo igro, ki
-#  ima za geslo naključno izbrano besedo iz seznama bazen_besed.
+def nova_igra():
+    return Igra(random.choice(bazen_besed))
