5571703d4b685199683cfb360ea4892b130cf833
Matej Bolta <matej.bolta@gmail.com>
2020-05-11 19:51:17 +0200

    izdelan tekstovni vmesnik

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,20 +1,39 @@
 import model
 
-trenutna_igra = model.nova_igra()
-
 def izpis_poraza(igra):
     return f'IZGUBIL SI, geslo je bilo: {igra.geslo}'
     
 def izpis_zmage(igra):
     return f'ZMAGAL SI, geslo je bilo: {igra.geslo}, potreboval si {len(igra.napacne_crke())} ugibov'
 
-def izpis_igra(igra):
-    text = (
-        f'Stanje gesla: {igra.pravilni_ugibi()}\n'
+def izpis_igre(igra):
+    tekst = (
+        f'Stanje gesla: {igra.pravilni_del_gesla()}\n'
         f'Imaš še {model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak()} možnosti za napako.'
     )
+    return tekst
 
 def zahtevaj_vnos():
     return input('Vpiši naslednjo črko: ')
 
-print(izpis_poraza(trenutna_igra))
+def pozeni_vmesnik():
+    # Naredimo novo igro
+    trenutna_igra = model.nova_igra()
+
+    while True:
+        # Pokažemo mu stanje
+        print(izpis_igre(trenutna_igra))
+
+        crka = zahtevaj_vnos()
+
+        trenutna_igra.ugibaj(crka)
+
+        if trenutna_igra.je_zmaga():
+            print(izpis_zmage(trenutna_igra))
+            return  # Skočimo ven
+        
+        if trenutna_igra.je_poraz():
+            print(izpis_poraza(trenutna_igra))
+            return # Skočimo ven
+
+pozeni_vmesnik()
