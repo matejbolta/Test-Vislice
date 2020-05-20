c9c650425b68b40d842c8f2eccfc4f81b73f724a
Matej Bolta <matej.bolta@gmail.com>
2020-05-20 19:27:25 +0200

    error fix

--- vislice(spletni).py
+++ vislice(spletni).py
@@ -19,16 +19,19 @@ vislice = model.Vislice()
 def index():
     return bottle.template('index.tpl')
 
+
 @bottle.post('/igra/')
 def nova_igra():
     id_nove_igre = vislice.nova_igra()
     bottle.redirect(f'/igra/{id_nove_igre}/')
+
 
 @bottle.get('/igra/<id_igre:int>/')
 def pokazi_igro(id_igre):
     igra, poskus = vislice.igre[id_igre]
 
     return bottle.template('igra.tpl', igra=igra, poskus=poskus, id_igre=id_igre)
+
 
 @bottle.post('/igra/<id_igre:int>/')
 def ugibaj(id_igre):
@@ -37,6 +40,7 @@ def ugibaj(id_igre):
     vislice.ugibaj(id_igre, crka)
 
     bottle.redirect(f'/igra/{id_igre}/')
+
 
 @bottle.get('/img/<slika>/')
 def serve_pictures(slika):
